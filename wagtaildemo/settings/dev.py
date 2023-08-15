972819eabc17ef804a975ca83113303836ca1564
mildfire <mildfire@gmail.com>
2023-08-15 19:50:37 +0500

    CodeRed file structure

---
+++ wagtaildemo/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-3%k!a)k%_t&9$cd2xrijuvxjnij-341*1on_@qb*@*c!m@nwlv"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
