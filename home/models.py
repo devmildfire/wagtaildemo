5e8b1f01b719443c6b4f44298322d4cbe6f824b6
Mildfire <mildfire@gmail.com>
2023-08-20 22:21:26 +0500

    get route

--- home/models.py
+++ home/models.py
@@ -3,9 +3,10 @@ from django.db import models
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel, InlinePanel
+from wagtail.contrib.routable_page.models import RoutablePageMixin, route
+from django.http import JsonResponse
 
-
-class HomePage(Page):
+class HomePage(RoutablePageMixin, Page):
     intro = RichTextField(null=True, blank=True)
     body = RichTextField(null=True, blank=True)
 
@@ -18,3 +19,10 @@ class HomePage(Page):
 
         verbose_name = 'Home Catalog Index Page'
         verbose_name_plural = 'Home Catalog Index Pages'
+    
+    @route(r'^sendcoffee/$')
+    def the_sendcoffee_page(self, request, *args, **kwargs):
+        context = self.get_context(request, *args, **kwargs)
+        context['coffeetest'] = 'This_is_a_coffee_test'
+        test_response = { "coffee_count" : 13 }
+        return JsonResponse(test_response, safe=False)
