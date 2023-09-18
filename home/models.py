05d3f35235eb9aca99b1000b8faa2a4b3ca870d6
devmildfire <97608157+devmildfire@users.noreply.github.com>
2023-09-18 10:13:51 +0000

    New home page of coffee statistics

--- home/models.py
+++ home/models.py
@@ -10,7 +10,6 @@ from .lookup import LookUp
 # from django.utils import timezone
 # from datetime import datetime
 
-
 class Coffeeman(models.Model):
   card_number = models.CharField(max_length=100,
                                  null=False,
@@ -45,7 +44,30 @@ class Coffeeman(models.Model):
 class HomePage(RoutablePageMixin, Page):
   intro = RichTextField(null=True, blank=True)
   body = RichTextField(null=True, blank=True)
+
   # view_count = models.PositiveBigIntegerField(default=0)
+  def get_context(self, request, *args, **kwargs):
+    context = super().get_context(request, *args, **kwargs)
+
+    coffeemen_names = []
+    for coffeeman in Coffeeman.objects.all():
+      if coffeeman.user_name not in coffeemen_names:
+        coffeemen_names.append(coffeeman.user_name)
+
+    print("names of all coffeemen are... ", coffeemen_names)
+
+    latest_records = []
+
+    for coffeeman in coffeemen_names:
+      latest_record = Coffeeman.objects.filter(
+        user_name=coffeeman).order_by('-time_stamp').first()
+      latest_record_time = latest_record.time_stamp
+      latest_records.append(latest_record)
+
+    context['latest_records'] = latest_records
+    context['update_time'] = latest_record_time
+
+    return context
 
   content_panels = Page.content_panels + [
     FieldPanel('intro'),
@@ -59,21 +81,12 @@ class HomePage(RoutablePageMixin, Page):
 
   @route(r'^sendcoffee/$')
   def the_sendcoffee_page(self, request, *args, **kwargs):
-    context = self.get_context(request, *args, **kwargs)
-    context['coffeetest'] = 'This_is_a_coffee_test'
 
     card_number = request.GET.get('card_number', None)
     user_name = request.GET.get('user_name', None)
     coffee_count = request.GET.get('coffee_count', None)
     coffee_pool = request.GET.get('coffee_pool', None)
     # time_stamp = datetime.now()
-
-    lookup_Objects = LookUp.objects.all()
-    print("All LookUp objects: ", lookup_Objects)
-
-    lookup_Objects_filtered = LookUp.objects.all().filter(
-      user_name="User_8a61b80")
-    print("User_8a61b80 LookUp object: ", lookup_Objects_filtered)
 
     user_readable_name = LookUp.objects.filter(user_name=user_name).first()
     # readable_name_value = getattr(user_readable_name, 'readable_name')
@@ -88,6 +101,7 @@ class HomePage(RoutablePageMixin, Page):
       coffee_pool=coffee_pool,
       # time_stamp=time_stamp
     )
+
     print("new coffeeman info: ", newCoffeeMan)
 
     # for creating new Coffeeman object use:
@@ -98,32 +112,10 @@ class HomePage(RoutablePageMixin, Page):
     # Coffeeman.objects.filter(card_number="AAAA").delete()
 
     # check if there is a Coffeeman with a given card number
-    checkCoffeeman = Coffeeman.objects.filter(card_number=card_number)
-    print("CHECKED coffeeman object is... ", checkCoffeeman)
-    print("CHECKED coffeeman object count... ", checkCoffeeman.count())
+    # checkCoffeeman = Coffeeman.objects.filter(card_number=card_number)
+    # print("CHECKED coffeeman object is... ", checkCoffeeman)
+    # print("CHECKED coffeeman object count... ", checkCoffeeman.count())
 
-    coffeemen = Coffeeman.objects.all()
-    print("coffeeman object is... ", coffeemen)
+    response = {"OK": True}
 
-    test_response = []
-
-    for man in coffeemen:
-      print("coffeeman name: ", man.user_name)
-      print("coffeeman card: ", man.card_number)
-      print("coffeeman coffee count: ", man.coffee_count)
-      print("coffeeman coffee pool: ", man.coffee_pool)
-      test_response.append({
-        "card_number": man.card_number,
-        "user_name": man.user_name,
-        "coffee_count": man.coffee_count,
-        "coffee pool": man.coffee_pool
-      })
-
-    # self.view_count = self.view_count + 1
-    # self.save()
-    # numCoffee = self.view_count
-    # print("new coffee count is... ", numCoffee)
-
-    # test_response = {"coffee_count": numCoffee}
-
-    return JsonResponse(test_response, safe=False)
+    return JsonResponse(response, safe=False)
